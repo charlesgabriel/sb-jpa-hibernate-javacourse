0bd7a98dc81767aaf06ee811534ec73e4db509b8
Charles Gabriel L Carmo <charlesgabriel@hotmail.com.br>
2026-03-05 20:03:08 -0300

    Project creation

---
+++ src/main/java/com/dev/sb_jpa_hibernate/SbJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.dev.sb_jpa_hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbJpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbJpaHibernateApplication.class, args);
+	}
+
+}
