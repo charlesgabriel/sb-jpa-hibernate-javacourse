218ebe17a504887ffda232c9bfb6eae57a43b9dc
Charles Gabriel L Carmo <charlesgabriel@hotmail.com.br>
2026-03-06 15:29:00 -0300

    Category entity, repository, resource and service creation

---
+++ src/main/java/com/dev/sb_jpa_hibernate/repositories/CategoryRepository.java
@@ -0,0 +1,8 @@
+package com.dev.sb_jpa_hibernate.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dev.sb_jpa_hibernate.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
