f8700afbd26fa0c98eb45619f23908100c932ec3
Charles Gabriel L Carmo <charlesgabriel@hotmail.com.br>
2026-03-06 11:08:02 -0300

    OrderStatus enum creation

--- src/main/java/com/dev/sb_jpa_hibernate/entities/Order.java
+++ src/main/java/com/dev/sb_jpa_hibernate/entities/Order.java
@@ -3,6 +3,7 @@ package com.dev.sb_jpa_hibernate.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
+import com.dev.sb_jpa_hibernate.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.Entity;
@@ -20,9 +21,9 @@ public class Order implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	@JsonFormat(shape = JsonFormat.Shape.STRING, 
-			pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
+	private Integer orderStatus;
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
@@ -30,9 +31,11 @@ public class Order implements Serializable {
 	public Order() {
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
+		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -50,6 +53,16 @@ public class Order implements Serializable {
 
 	public void setMoment(Instant moment) {
 		this.moment = moment;
+	}
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
 	}
 
 	public User getClient() {
