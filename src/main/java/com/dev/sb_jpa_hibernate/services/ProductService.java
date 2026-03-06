d8c1d2a166b3581c3a5b6e9a4206dea10ba629f4
Charles Gabriel L Carmo <charlesgabriel@hotmail.com.br>
2026-03-06 16:08:19 -0300

    Product entity, repository, resource and service creation

---
+++ src/main/java/com/dev/sb_jpa_hibernate/services/ProductService.java
@@ -0,0 +1,25 @@
+package com.dev.sb_jpa_hibernate.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dev.sb_jpa_hibernate.entities.Product;
+import com.dev.sb_jpa_hibernate.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	@Autowired
+	private ProductRepository repository;
+
+	public List<Product> findAll() {
+		return repository.findAll();
+	}
+
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+}
