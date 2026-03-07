478bae3d171c8c60db54ce3f9ea861ab9b9adc3e
Charles Gabriel L Carmo <charlesgabriel@hotmail.com.br>
2026-03-07 07:31:53 -0300

    Payment entity and one-to-one association creation

--- src/main/java/com/dev/sb_jpa_hibernate/config/TestConfig.java
+++ src/main/java/com/dev/sb_jpa_hibernate/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.dev.sb_jpa_hibernate.entities.Category;
 import com.dev.sb_jpa_hibernate.entities.Order;
 import com.dev.sb_jpa_hibernate.entities.OrderItem;
+import com.dev.sb_jpa_hibernate.entities.Payment;
 import com.dev.sb_jpa_hibernate.entities.Product;
 import com.dev.sb_jpa_hibernate.entities.User;
 import com.dev.sb_jpa_hibernate.entities.enums.OrderStatus;
@@ -74,5 +75,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+
+		orderRepository.save(o1);
 	}
 }
